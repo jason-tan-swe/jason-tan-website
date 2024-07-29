7f7879a265fa530a05b6661542a1cd59e472b5c7
Jason Tan <37030567+AeGGIndie@users.noreply.github.com>
2024-07-28 21:30:44 -0400

    chore: fix loading

--- app/loading.tsx
+++ app/loading.tsx
@@ -3,7 +3,7 @@
 import Lottie from "lottie-react";
 import LoadingAnimation from "@/public/loadingAnimation.json";
 
-export default function Loading({ className = "" }: { className: string }) {
+export default function Loading({ className }: { className?: string }) {
   return (
     <div className={`${className} flex flex-col items-center justify-center`}>
       <Lottie height={350} animationData={LoadingAnimation} loop autoplay />
